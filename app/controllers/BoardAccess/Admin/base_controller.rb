1686d7bab4dfe88486afb259512b78c4770d923a
André Barbosa <albmail88@gmail.com>
2013-03-04 16:21:34 +0000

    cleaned some confusion on the admin/base_controller.rb filecleaned some confusion on the admin/base_controller.rb file..

--- app/controllers/BoardAccess/Admin/base_controller.rb
+++ app/controllers/BoardAccess/Admin/base_controller.rb
@@ -3,15 +3,8 @@ require_relative '../board_access_base_controller.rb'
 module BoardAccess
   module Admin
     class BaseController < BoardAccess::BoardAccessBaseController
-      def destroy
-        @board.destroy
-        redirect_to redirect_path
-      end
+      
     private
-      def authenticate_user!
-        permission_denied unless user_signed_in?
-      end      
-
       def grant_access?
         @board.is_admin? current_user
       end      
